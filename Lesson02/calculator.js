cb616619b115b6c4e0cace9d73e2b52f708f5522
Ida Mae Jones <flygirlIMJ@gmail.com>
2020-04-09 15:47:26 +0000

    JS101 Lesson 02 2020 0409b

--- Lesson02/calculator.js
+++ Lesson02/calculator.js
@@ -1,3 +1,4 @@
+const readline = require('readline-sync');
 // Ask the user for the first number.
 // Ask the user for the second number.
 // Ask the user for an operation to perform.
@@ -5,3 +6,49 @@
 // Print the result to the terminal.
 
 console.log('Welcome to Calculator!');
+
+console.log('What is the first number?');
+let number1 = readline.question();
+
+console.log('What is the second number?');
+let number2 = readline.question();
+
+console.log('What operation would you like to perform?\n' + 
+            '+  -  *  /  **');
+let operation = readline.question();
+
+let output;
+/*
+if (operation === '1') { // '1' represents addition
+  output = Number(number1) + Number(number2);
+} else if (operation === '2') { // '2' represents subtraction
+  output = Number(number1) - Number(number2);
+} else if (operation === '3') { // 3 represents multiplication
+  output = Number(number1) * Number(number2);
+} else if (operation === '4') { // 4 represents division
+  output = Number(number1) / Number(number2);
+}
+*/
+
+switch (operation) {
+    case '+':
+        output = Number(number1) + Number(number2);
+        break;
+    case '-':
+        output = Number(number1) - Number(number2);
+        break;
+    case '*':
+        output = Number(number1) * Number(number2);
+        break;
+    case '/':
+        output = Number(number1) / Number(number2);
+        break;
+    case '^':
+        output = Number(number1) ** Number(number2);
+        break;
+    default:
+        console.log('Please enter one of the following mathematical\n' +
+                    ' +  -  *  / ^')
+}
+
+console.log(`The result is: ${output}`);
