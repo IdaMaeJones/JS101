61cd2e079963779a10a733854feb371191bb7825
Ida Mae Jones <flygirlIMJ@gmail.com>
2020-05-16 11:19:18 +0000

    Launch School, JS101, Lesson 03 Easy2 exercises baseline

--- Lesson03/Easy2.js
+++ Lesson03/Easy2.js
@@ -50,10 +50,56 @@ dinosaur.`;
 console.log(advice.replace('important', 'urgent'));
 qs += 1;
 ans += 1;
-print('end');
+console.log(" ");
+console.log(`Other examples of string replacement include using reular 
+expressions as follows:`);
+console.log(`let str10 = "Welcome message: Hello World! hello world! Hello, 
+hello, hello"; `)
+let str10 = "Welcome message: Hello World! hello world! Hello, hello, hello"; 
+console.log(`str10.replace('hello', 'hi'): ` + str10.replace('hello', 'hi')); 
+console.log(`str10.replace(/hello/, 'hi'): ` + str10.replace(/hello/, 'hi')); 
+console.log(`str10.replace(/hello/g, 'hi'): ` + str10.replace(/hello/g, 'hi')); 
+console.log(`str10.replace(/hello/gi, 'hi'): ` + str10.replace(/hello/gi, 'hi')); 
+console.log(`str10 remains unchanged: ` + str10);
+
+console.log(' ');
+console.log(`The same can be achieved using objects to map replacements and 
+functions in .replace:`);
+function getObjKeysValues(myObj){
+  var keysAndValues = [];
+  var value;
+  // keys.push(Object.getOwnPropertyNames(mapObj));
+  for(var key in myObj) {
+    if (Object.prototype.hasOwnProperty.call(myObj, key)) {
+      value = key + `:` + myObj[key];
+      keysAndValues.push(value);
+    }
+  }
+  return keysAndValues;
+}
+var mapObj = {cat:"kitty", dog:"pooch", goat:"billy"};
+console.log(`mapObj is: ` + getObjKeysValues(mapObj));
+
+var re = new RegExp(Object.keys(mapObj).join("|"),"gi");
+console.log(`regular expression used is: ` + re);
+
+let str12 = "I have a cat, a dog, a mouse, and a goat.";
+console.log(`str12 original: ` + str12);
+
+str12 = str12.replace(re, function(matched){return mapObj[matched];});
+console.log(`str12 is now: ` + str12);
+
+mapObj.mouse = "snake";
+console.log(`mapObj is: ` + getObjKeysValues(mapObj));
+
+re = RegExp(Object.keys(mapObj).join("|"),"gi");
+console.log(`regular expression used is: ` + re);
+
+str12 = str12.replace(re, function(matched){return mapObj[matched];});
+console.log(`str12 is now: ` + str12);
 
 /*
-// Qs. 10
+// Qs. 2
 print('qs');
 buffer = ` `;
 print();
