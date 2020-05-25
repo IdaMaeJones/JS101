c71b172957afa6085d712117f16c8a411ab23d7e
Ida Mae Jones <flygirlIMJ@gmail.com>
2020-05-25 20:30:10 +0000

    Launch School, JS101, Lesson 03, Medium1 exercises, Qs. 5 completed.

--- Lesson03/Medium1.js
+++ Lesson03/Medium1.js
@@ -38,29 +38,56 @@ function print(arg) {
 
 
 // START
-/*
 // Qs. 1
 QS = `qs` + qs;
 print('qs');
-buffer = ` `;
+buffer = `Let's do some "ASCII Art": a stone-age form of nerd artwork from back
+in the days before computers had video screens. Write a program that creates the
+following output 10 times, with each line indented 1 space to the right of the 
+line above it:
+The Flintstones Rock!
+ The Flintstones Rock!
+  The Flintstones Rock!`;
 print();
 print('ans');
 buffer = ` `;
 print();
 print('code');
-buffer = ` `;
+buffer = `let message = "The Flintstones Rock!";
+let padding = '';
+for (let count = 0; count < 10; count++) {
+  padding = padding + ' ';
+  console.log(padding + message);
+};
+//Another alternative
+for (let count = 0; count < 10; count++) {
+  console.log(" ".repeat(count+1) + message);
+};`;
 print();
 print('output');
+let message = "The Flintstones Rock!";
+let padding = '';
+let repetitions = 10;
+for (let count = 0; count < repetitions; count++) {
+  padding = padding + ' ';
+  console.log(padding + message);
+}
+padding = ' ';
+for (let count = 0; count < repetitions; count++) {
+  console.log(padding.repeat(count+1) + message);
+}
 qs += 1;
 ans += 1;
 print('end');
 
 
-/*
 // Qs. 2
 QS = `qs` + qs;
 print('qs');
-buffer = ` `;
+buffer = `Starting with the string:
+let munstersDescription = "The Munsters are creepy and spooky.";
+Return a new string that swaps the case of all of the letters:
+\`tHE mUNSTERS ARE CREEPY AND SPOOKY.\``;
 print();
 print('ans');
 buffer = ` `;
@@ -69,60 +96,128 @@ print('code');
 buffer = ` `;
 print();
 print('output');
+let munstersDescription = "The Munsters are creepy and spooky.";
+function switchCase(str) {
+  let splitCharacters = str.split('');
+  let modifiedString = '';
+  for (let count = 0; count < splitCharacters.length; count++) {
+    if (str.charAt(count).toUpperCase() == str.charAt(count)) {
+      modifiedString = modifiedString + str.charAt(count).toLowerCase();
+    } else {
+      modifiedString = modifiedString + str.charAt(count).toUpperCase();
+    }
+  }
+  console.log(`Original string: ${str}`);
+  return modifiedString;
+}
+console.log(switchCase(munstersDescription));
 qs += 1;
 ans += 1;
 print('end');
 
 
-/*
 // Qs. 3
 QS = `qs` + qs;
 print('qs');
-buffer = ` `;
+buffer = `Alan wrote the following function, which was intended to return all of
+the factors of 'number':
+function factors(number) {
+  let divisor = number;
+  let factors = [];
+  do {
+    if (number % divisor === 0) {
+      factors.push(number / divisor);
+    }
+    divisor -= 1;
+  } while (divisor !== 0);
+  return factors;
+}
+Alyssa noticed that this code would fail when the input is 0 or a negative 
+number, and asked Alan to change the loop. How can he make this work without 
+using a do/while loop? Note that we're not looking to find the factors for 0 or
+negative numbers, but we want to handle it gracefully instead of raising an 
+exception or going into an infinite loop.
+Bonus: What is the purpose of number % divisor === 0 in that code?`;
 print();
 print('ans');
-buffer = ` `;
+buffer = `Use a while-loop instead of do-while. The do-while loop ensures that 
+the code in the loop is executed at least one time before the loop condition is
+checked. The purpose of the number % divisor === 0 is to see if the divisor is a
+factor of the number as there is no remainder resulting from the division by a
+factor.`;
 print();
-print('code');
-buffer = ` `;
-print();
-print('output');
+//print('code');
+//buffer = ` `;
+//print();
+//print('output');
 qs += 1;
 ans += 1;
 print('end');
 
 
-/*
 // Qs. 4
 QS = `qs` + qs;
 print('qs');
 buffer = ` `;
 print();
 print('ans');
-buffer = ` `;
+buffer = `Yes, there is a difference. The first implementation mutates the input
+but the second implementation does not and hence it is not a valid solution for
+the problem of implementing a rolling buffer. The new element is not added to 
+the buffer in the second implementation.`
 print();
-print('code');
-buffer = ` `;
-print();
+//print('code');
+//buffer = ` `;
+//print();
 print('output');
+buffer ='';
+function addToRollingBuffer1(buffer, maxBufferSize, newElement) {
+  buffer.push(newElement);
+  if (buffer.length > maxBufferSize) {
+    buffer.shift();
+  }
+  return buffer;
+}
+function addToRollingBuffer2(buffer, maxBufferSize, newElement) {
+  buffer = buffer.concat(newElement);
+  if (buffer.length > maxBufferSize) {
+    buffer.shift();
+  }
+  return buffer;
+}
+let myBuffer = [];
+console.log(`addToRollingBuffer1 ...`);
+myBuffer = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(`Buffer of max size 10: ${myBuffer} to which element 'b' is added.`);
+addToRollingBuffer1(myBuffer, 10, 'b');
+console.log(myBuffer);
+console.log(`addToRollingBuffer2 ...`);
+myBuffer = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(`Buffer of max size 10: ${myBuffer} to which element 'b' is added.`);
+addToRollingBuffer2(myBuffer, 10, 'b');
+console.log(myBuffer);
 qs += 1;
 ans += 1;
 print('end');
 
 
-/*
 // Qs. 5
 QS = `qs` + qs;
 print('qs');
-buffer = ` `;
+buffer = `What will the following two lines of code output?
+console.log(0.3 + 0.6);
+console.log(0.3 + 0.6 === 0.9);`;
 print();
 print('ans');
-buffer = ` `;
+buffer = `The first line will output 0.89999... and the second the boolean false
+due to the use of floating point arithmetic that can yield unexpected results.`;
 print();
-print('code');
-buffer = ` `;
-print();
+//print('code');
+//buffer = ` `;
+//print();
 print('output');
+console.log(0.3 + 0.6);
+console.log(0.3 + 0.6 === 0.9);
 qs += 1;
 ans += 1;
 print('end');
