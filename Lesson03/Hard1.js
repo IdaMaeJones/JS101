bb84ee70b21f25c14d434c4e9c963bf73c56059b
Ida Mae Jones <flygirlIMJ@gmail.com>
2020-05-26 14:59:19 +0000

    Launch School, JS101, Lesson 03, Hard1 exercises, Qs 1

---
+++ Lesson03/Hard1.js
@@ -0,0 +1,167 @@
+// Launch School
+// JS 101 Lesson 3: Practice Problems
+// Medium1
+var QS = '';
+//
+// COMMON DISPLAY CODE
+const QUESTION = 'QUESTION';
+const ANSWER = 'ANSWER';
+var buffer = '';
+var divider = "==== ==== ==== ==== ====";
+var qs = 1;
+var ans = 1;
+var code = '.......... CODE ..........';
+var output = '.......... OUTPUT ..........';
+function print(arg) {
+  switch (arg) {
+    case 'qs':
+      console.log(`${divider}\n${QUESTION} ${qs}: `);
+      break;
+    case 'ans':
+      console.log(`${divider}\n${ANSWER} ${ans}: `);
+      break;
+    case 'code':
+      console.log(`${code}`);
+      break;
+    case 'end':
+      console.log(` `);
+      break;
+    case 'output':
+      console.log(`${output}`);
+      break;
+    default:
+      console.log(`${buffer}`);
+      break;
+  }
+}
+// END DISPLAY CODE
+
+
+// START
+// Qs. 1
+QS = `qs` + qs;
+print('qs');
+buffer = `Will the following functions return the same results?
+
+function first() {
+  return {
+    prop1: "hi there"
+  };
+}
+
+function second() {
+  return
+  {
+    prop1: "hi there"
+  };
+}
+
+console.log(first());
+console.log(second());`;
+print();
+print('ans');
+buffer = `No, they will not return the same result. The { that is on the line
+next to return in second() causes problems. JavaScript inserts one if there is
+not a semi-colon where one is expected (as semicolons are optional at end of
+line) and this causes the function second() to return undefined.`;
+print();
+// print('code');
+// buffer = ` `;
+// print();
+print('output');
+function first() {
+  return {
+    prop1: "hi there"
+  };
+}
+
+function second() {
+  return
+  {
+    prop1: "hi there"
+  };
+}
+
+console.log(first());
+console.log(second());
+qs += 1;
+ans += 1;
+print('end');
+
+
+// Qs. 2
+QS = `qs` + qs;
+print('qs');
+buffer = ` `;
+print();
+print('ans');
+buffer = ` `;
+print();
+print('code');
+buffer = ` `;
+print();
+print('output');
+qs += 1;
+ans += 1;
+print('end');
+
+
+/*
+// Qs. 3
+QS = `qs` + qs;
+print('qs');
+buffer = ` `;
+print();
+print('ans');
+buffer = ` `;
+print();
+print('code');
+buffer = ` `;
+print();
+print('output');
+qs += 1;
+ans += 1;
+print('end');
+
+
+/*
+// Qs. 4
+QS = `qs` + qs;
+print('qs');
+buffer = ` `;
+print();
+print('ans');
+buffer = ` `;
+print();
+print('code');
+buffer = ` `;
+print();
+print('output');
+qs += 1;
+ans += 1;
+print('end');
+
+
+/* TEMPLATE
+/*
+// Qs. 
+QS = `qs` + qs;
+print('qs');
+buffer = ` `;
+print();
+print('ans');
+buffer = ` `;
+print();
+print('code');
+buffer = ` `;
+print();
+print('output');
+qs += 1;
+ans += 1;
+print('end');
+
+
+*/
+
+// END
+/* ==== ==== ==== ==== */
