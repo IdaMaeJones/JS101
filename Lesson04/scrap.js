1970b57424a0e1814be2d261c85e705e7a65bc51
Ida Mae Jones <flygirlIMJ@gmail.com>
2020-06-22 20:31:50 +0000

    Launch School, JS101, Lesson 04, experiment with code

--- Lesson04/scrap.js
+++ Lesson04/scrap.js
@@ -1,4 +1,25 @@
+/*
 let myObject = {'first fruit':'apple or pear', 'last fruit':''};
 console.log(myObject);
 console.log(myObject['first fruit']);
 console.log(typeof myObject);
+*/
+
+// names.js
+let names = ['Chris', 'Kevin', 'Naveed', 'Pete', 'Victor'];
+console.log(names);
+let maxLength = names.length;
+let upperNames = [];
+let index = 0;
+
+while (index < maxLength) {
+  names[index] = names[index].toUpperCase();
+  /*
+  let upperCaseName = names.pop().toUpperCase();
+  upperNames.push(upperCaseName);
+  */
+  index += 1;
+}
+// names = upperNames.reverse();
+// console.log(upperNames); // ['CHRIS', 'KEVIN', 'NAVEED', 'PETE', 'VICTOR']
+console.log(names); // ['CHRIS', 'KEVIN', 'NAVEED', 'PETE', 'VICTOR']
