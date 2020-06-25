6f0505b19b2c257a59b3bcfef8f0d66a8d405808
Ida Mae Jones <flygirlIMJ@gmail.com>
2020-06-25 11:28:09 +0000

    Launch School, JS101, Lesson 04, Quiz 1 prep

--- Lesson04/scrap.js
+++ Lesson04/scrap.js
@@ -1,3 +1,87 @@
+let arr1 =[1, 2, 3, 4, 5];
+let result = arr1.filter(num => num > 0).pop();
+result;
+console.log(result);
+console.log(arr1);
+
+/*
+result = [1, 2, 3, 4, 5].map(num => num[-1]);
+result;
+console.log(result);
+
+/*
+result = [1, 2, 3, 4, 5].forEach(num => num +1);
+result;
+console.log(result);
+result = [1, 2, 3, 4, 5].filter(num => num +1);
+console.log(result);
+result;
+result = [1, 2, 3, 4, 5].map(num => num +1);
+result;
+console.log(result);
+/*
+result = ['a', 'b', 'c'].forEach(function(item) {
+  console.log(item);
+});
+result;
+
+result = ['a', 'b', 'c'].map(function(item) {
+  console.log(item);
+});
+result;
+
+result = ['a', 'b', 'c'].filter(function(item) {
+  console.log(item);
+});
+result;
+
+
+/*
+let cat = "Butterscotch";
+console.log(cat[8]);
+console.log(cat[cat.indexOf('o')]);
+
+let dog = 'fido';
+let letters = dog.split('');
+letters[0] = letters[0].toUpperCase();
+dog = letters.join('');
+console.log(dog);
+
+let text = "This is a string";
+let arr = ['This', 'is', 'a', 'string'];
+let obj = { 'This is': 'a string' };
+// console.log(obj.'This is');
+console.log(letters[3.14]);
+// console.log(obj.keys().includes('x'));
+console.log(obj['x']);
+console.log(Object.keys(obj).includes('x'));
+console.log(obj.x);
+
+console.log(text.split(/ /));
+console.log(arr[-1]);
+
+let colors = ['green', 'blue', 'red'];
+let result = colors.map(word => word.toUpperCase());
+console.log(result);
+result = colors.map(word => word.toUpperCase());
+console.log(result);
+
+result = [1, 2, 3, 4, 5].map(num => num +1);
+console.log(result);
+result = [1, 2, 3, 4, 5].forEach(num => num +1);
+console.log(result);
+
+result = ['a', 'b', 'c'].forEach(function(item) {
+  console.log(item);
+});
+result;
+
+result = ['a', 'b', 'c'].map(function(item) {
+  console.log(item);
+});
+result;
+
+
 
 
 
