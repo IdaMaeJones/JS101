9203bbc6d3a15314956a41389a5cd73b85544e9f
Ida Mae Jones <flygirlIMJ@gmail.com>
2020-10-15 12:25:35 +0000

    Launch School, JS101, Small Problems, Easy2, ver. 6

--- SmallProbs/Easy2.js
+++ SmallProbs/Easy2.js
@@ -36,7 +36,7 @@ function print(arg) {
 }
 // END DISPLAY CODE
 
-
+/*
 // START
 // Qs. 1
 QS = `qs` + qs;
@@ -74,7 +74,15 @@ Code:
   `;
 print();
 function greetings(arg1, arg2) {
-  let greet = `Hello, ___ ! Nice to have a ___ around.` ;
+  let greet = `Hello,` ;
+  greet += arg1.join(" ") + "! Nice to have a";
+  let titleArray = Object.getOwnPropertyNames(arg2);
+  let arg2Property ='';
+  for (var i = 0; i < titleArray.length; i++) {
+    arg2Property = titleArray[i];
+    greet = greet + ' ' + arg2[`${arg2Property}`];
+  }
+  greet += ' around.';
   return greet;
 }
 
@@ -83,7 +91,7 @@ buffer = ` `;
 print();
 print('output');
 console.log(
-  greetings(["John", "Q", "Doe"], {title: "Master", occupation: "Plumber"})
+  greetings(["John", "Q", "Doe"], {title: "Master", occupation: "Plumber", grade: "Grade 2"})
 );
 qs += 1;
 ans += 1;
@@ -93,62 +101,213 @@ print('end');
 // Qs. 2
 QS = `qs` + qs;
 print('qs');
-buffer = `Write a function that computes the sum of all numbers between 1 and 
-some other number, inclusive, that are multiples of 3 or 5. For instance, if the
-supplied number is 20, the result should be 98 
-(3 + 5 + 6 + 9 + 10 + 12 + 15 + 18 + 20).
-You may assume that the number passed in is an integer greater than 1..`;
+buffer = `Write a program that will ask for user's name. The program will then 
+greet the user. If the user writes "name!" then the computer yells back to the 
+user.
+What is your name? Bob
+Hello Bob.
+What is your name? Bob!
+HELLO BOB. WHY ARE WE SCREAMING?`;
 print();
 print('ans');
-buffer = `multisum(arg1) - Returns true if arg1 leap year, false otherwise.`;
+buffer = `greetUser(arg) - Greet user as appropriate.`;
 print();
 print('code');
 buffer = `PEDAC
-Problem: Sum all multiples of 3 or 5 up to a given input.
+Problem: Echo back the input name. All caps and ask a question if ! at end.
 Examples/Edge & Test Cases:
-multisum(3);       // 3
-multisum(5);       // 8
-multisum(10);      // 33
-multisum(1000);    // 234168
+What is your name? Bob
+Hello Bob.
+What is your name? Bob!
+HELLO BOB. WHY ARE WE SCREAMING?
 Data Structure:
-  multisum(arg);
-  arg - number (assume integer greater than 1)
-  answerSum - number
+  none
 Algorithm:
- for i = 0, i <= arg, i++
-  i%3 === 0 - add i to answer
-  if not i%3 === 0, i%5 === 0 - add i to answer
+  if ! at end, all caps and qs
+    else normal greeting
 Code:
-  if i%3 != 0  // not div by 3, check 5
-      if i%5 != 0  // not div by 5
-        else add i to answer  // div by 5, add to answer
-    else add i to answer  // div by 3, add to answer
+  if arg.arg.length-1 is !  - all caps greeting
+    else regular greeting
   `;
 print();
-function multisum(arg) {
-  let answerSum = 0;
-  for (var i = arg; i >= 0; i--) {
-    if (i%3 != 0) {
-      if (i%5 != 0) {
-      } else {
-        answerSum += i ;
-      }
-    } else {
-      answerSum += i ;
-    }
+function greetUser() {
+  let rlSync = require('readline-sync');
+  let userName = rlSync.question("What is your name? ");
+  let idx = userName.length - 1 ;
+  if (userName[idx] != '!') {
+    console.log(`Hello ${userName}.`);
+  } else {
+    console.log(`HELLO ${userName.toUpperCase()}. WHY ARE WE SCREAMING?`);
   }
-  return answerSum;
 }
 
 
 buffer = ` `;
 print();
 print('output');
-let TEST_CASES = [2, 3, 5, 10, 1000] ;
-let TEST_SUMS = [0, 3, 8, 33, 234168];
-for (var i = TEST_SUMS.length; i--; ) {
-  console.log(`multisum(${TEST_CASES[i]}) is ${multisum(TEST_CASES[i])} ... ${TEST_SUMS[i]}.`);
+greetUser();
+greetUser();
+qs += 1;
+ans += 1;
+print('end');
+
+
+// Qs. 3
+QS = `qs` + qs;
+print('qs');
+buffer = `Create a function that takes two arguments, multiplies them together, 
+and returns the result.`;
+print();
+print('ans');
+buffer = `multiply(arg1, arg2) - Multiply and return result.`;
+print();
+print('code');
+buffer = `PEDAC
+Problem: multiply two arguments passed to function
+Example and Edge/Test Cases:
+  multiply(5, 3)
+Data Structures: result - number
+Algorithm: result = arg1 * arg2
+Code: ...
+`;
+print();
+function multiply(arg1, arg2) {
+  let result = arg1 * arg2;
+  return result;
 }
+
+
+buffer = ` `;
+print();
+print('output');
+console.log(`multiply(5, 3) returns ${multiply(5, 3)} === 15.`);
+
+qs += 1;
+ans += 1;
+print('end');
+
+
+// Qs. 4
+QS = `qs` + qs;
+print('qs');
+buffer = `Using the multiply() function from the "Multiplying Two Numbers" 
+problem, write a function that computes the square of its argument (the square 
+is the result of multiplying a number by itself).`;
+print();
+print('ans');
+buffer = `square(arg) - Square and return result.`;
+print();
+print('code');
+buffer = `PEDAC
+Problem: square argument passed to function
+Example and Edge/Test Cases:
+  square(5)
+  square(-8)
+Data Structures: result - number
+Algorithm: result = arg * arg
+Code: ...
+`;
+print();
+function square(arg) {
+  let result = multiply(arg, arg);
+  return result;
+}
+
+
+buffer = ` `;
+print();
+print('output');
+console.log(`square(5) returns ${square(5)} === 25.`);
+console.log(`square(-8) returns ${square(-8)} === 64.`);
+
+qs += 1;
+ans += 1;
+print('end');
+
+
+// Qs. 5
+QS = `qs` + qs;
+print('qs');
+buffer = `Write a program that prompts the user for two positive integers, and 
+then prints the results of the following operations on those two numbers: 
+addition, subtraction, product, quotient, remainder, and power. 
+Do not worry about validating the input.`;
+print();
+print('ans');
+buffer = `numberTricks() - Perform number operations on two inputs entered.`;
+print();
+print('code');
+buffer = `PEDAC
+Problem: perform number operations
+Example and Edge/Test Cases: 23, 17
+Data Structures: 
+Number - addition, subtraction, product, quotient, remainder, and power
+Code: ...
+`;
+print();
+function numberTricks() {
+  let rlSync = require("readline-sync");
+  let firstNumber = Number(rlSync.question("Enter the first number\n"), 10);
+  let secondNumber = Number(rlSync.question("Enter the second number\n"), 10);
+  let addition = firstNumber + secondNumber;
+  let subtraction = firstNumber - secondNumber;
+  let multiplication = firstNumber * secondNumber;
+  let division = Math.floor(firstNumber / secondNumber);
+  let quotient = firstNumber%secondNumber;
+  let power = Math.pow(firstNumber, secondNumber);
+  console.log(`${firstNumber} + ${secondNumber} = ${addition}`);
+  console.log(`${firstNumber} - ${secondNumber} = ${subtraction}`);
+  console.log(`${firstNumber} * ${secondNumber} = ${multiplication}`);
+  console.log(`${firstNumber} / ${secondNumber} = ${division}`);
+  console.log(`${firstNumber} % ${secondNumber} = ${quotient}`);
+  console.log(`${firstNumber} ** ${secondNumber} = ${power}`);
+}
+
+
+buffer = ` `;
+print();
+print('output');
+numberTricks();
+
+qs += 1;
+ans += 1;
+print('end');
+*/
+
+// Qs. 6
+QS = `qs` + qs;
+print('qs');
+buffer = `Write a function that returns the next to last word in the String 
+passed to it as an argument. Words are any sequence of non-blank characters.
+You may assume that the input String will always contain at least two words.`;
+print();
+print('ans');
+buffer = `penultimate() - return last but one word.`;
+print();
+print('code');
+buffer = `PEDAC
+Problem: return penultimate string in array of strings
+Example and Edge/Test Cases: "last word" and "Launch School is tedious!"
+Data Structures: 
+  Array - elements
+  String - result
+Code: ...
+`;
+print();
+function penultimate(arg) {
+  let elements = arg.split(' ');
+  let result = elements[elements.length - 2];
+  return result;
+}
+
+
+buffer = ` `;
+print();
+print('output');
+console.log(penultimate("last word") === "last"); // logs true
+console.log(penultimate("Launch School is tedious!") === "is"); // logs true
+console.log(`"last word" ... last === ${penultimate("last word")}`);
+console.log(`"Launch School is tedious!" ... is === ${penultimate("Launch School is tedious!")}`);
 qs += 1;
 ans += 1;
 print('end');
