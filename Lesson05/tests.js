0de15e9e54638cc587807bdeb66d53c51b19fb7f
Ida Mae Jones <flygirlIMJ@gmail.com>
2020-10-26 14:59:22 +0000

    Launch School, JS101, Lesson 5, mid ver. tests.js

--- Lesson05/tests.js
+++ Lesson05/tests.js
@@ -63,7 +63,7 @@ console.log(`Original array:`);
 console.log(numArray) ;
 console.log(`Descending sort array:`);
 console.log(numArray.slice().sort( (a, b) => b - a )) ;
-*/
+
 
 // Array of scores of 4 players
 let scores = [ [ 1, 4, 2 ], [ 3, 6, 4 ], [ 6, 8, 9 ], [ 0, 3, 2 ] ];
@@ -95,4 +95,43 @@ console.log(copyArr);
 console.log(`Modifiying copyArr by copyArr[1] = ['d']:`);
 copyArr[1] = ['d'];
 console.log(copyArr);
+console.log(`Original Array arr is same as before:`);
+console.log(arr);
+console.log(`Modifiying copyArr by copyArr[0].push(['d']);`);
+copyArr[0].push(['d']);
+console.log(copyArr);
+console.log(`Original Array arr is now:`);
+console.log(arr);
+*/
 
+/*
+let mixArray =[[[1], [2], [3], [4]], [['a'], ['b'], ['c']]] ;
+let retValue ;
+retValue = mixArray.map(element1 => {
+  return element1.map(element2 => {
+    return element2.filter(element3 => {
+      return element3.length > 0;
+    });
+  });
+});
+console.log(retValue);
+// => [ undefined, undefined ]
+
+// let array3 = [['a'], ['b'], ['c']];
+let array3 = ['a'];
+console.log(`${array3.forEach(element => {return element.length > 0;})}`);
+*/
+
+let obj1 = [{ a: 'ant', b: 'elephant' }, { c: 'cat', d: 'dog' }].filter(object => {
+  return Object.keys(object).every(key => object[key][0] === key);
+});
+
+console.log(obj1);
+// => [ { c: 'cat', d: 'dog' } ]
+
+let obj2 = [{ a: 'ant', b: 'elephant' }, { c: 'cat', d: 'dog' }].filter(object => {
+  return Object.keys(object).some(key => object[key][0] === key);
+});
+console.log(obj2);
+
+// => [ {a: 'ant', b: 'elephan'}, { c: 'cat', d: 'dog' } ]
