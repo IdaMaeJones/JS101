8c0cc6b45412b298c2c966517581a2f5ec212930
Ida Mae Jones <flygirlIMJ@gmail.com>
2020-11-04 01:59:01 +0000

    Launch School, JS101, Lesson 5, Practice Problems ver. 13

--- Lesson05/L05_Practice.js
+++ Lesson05/L05_Practice.js
@@ -547,7 +547,7 @@ console.log(arr40);
 qs += 1;
 ans += 1;
 print('end');
-*/
+
 
 // Qs. 12
 QS = `qs` + qs;
@@ -585,7 +585,44 @@ console.log(arr50);
 qs += 1;
 ans += 1;
 print('end');
+*/
 
+// Qs. 13
+QS = `qs` + qs;
+print('qs');
+buffer = `Given the following data structure, use a combination of methods, 
+including filter, to return a new array identical in structure to the original, 
+but containing only the numbers that are multiples of 3.
+let arr = [[2], [3, 5, 7], [9], [11, 15, 18]];
+`;
+print();
+print('ans');
+buffer = ` `;
+print();
+print('code');
+buffer = `let arr50 = [[2], [3, 5, 7], [9], [11, 15, 18]];
+let arr50New = [];
+
+arr50New = arr50.filter(num => num % 3 === 0);
+
+console.log(arr50New);
+console.log(arr50);
+// => [ [], [ 3 ], [ 9 ], [ 15, 18 ] ]
+`;
+print();
+let arr50 = [[2], [3, 5, 7], [9], [11, 15, 18]];
+let arr50New = [];
+
+arr50New = arr50.map(element => {
+  return element.filter(num => num % 3 === 0);
+});
+
+print('output');
+console.log(arr50New);
+console.log(arr50);
+qs += 1;
+ans += 1;
+print('end');
 
 
 
